605ac38fa60028f58eb172d0815abec23285f3e4
Sourav Mandal <sm227465@gmail.com>
2023-06-10 21:04:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const compression = require('compression');
+const xss = require('xss-clean');
+const ytdl = require('ytdl-core');
+
+// setting path for .env file
+dotenv.config({ path: './config.env' });
+
+// calling express function and store into a variable
+const app = express();
+
+// setting templating view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// i) Implemented cors
+app.use(cors());
+app.options('*', cors());
+
+// ii) Set security HTTP headers
+app.use(helmet());
+
+// iii) Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// iv) Limiting request from same IP (its help us from Bruteforce and DOS/DDOS attacks)
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from your IP, please try again in an hour.',
+});
+app.use('/api', limiter);
+
+// v) Body parser (reading data from body into req.body) and set limit to 10kb on creating and updating documents
+app.use(express.json({ limit: '10kb' }));
+
+// viii) Data sanitization against XSS (Cross-site scripting)
+app.use(xss());
+
+// ix) Prevent parameter pollution (pass all the params inside whitelist array which you want to allow in req.query to avoid unnecessary params by a malicious user)
+app.use(hpp({ whitelist: [] }));
+
+// x) Compressing
+app.use(compression());
+
+app.get('/api/v1/yt/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide a YouTube video ID',
+    });
+  }
+
+  //   console.log(id);
+
+  const result = ytdl.validateID(id);
+
+  if (!result) {
+    return res.status(404).json({
+      success: false,
+      message: `Sorry! Can not find YouTube video with this ID: ${id}`,
+    });
+  }
+
+  const info = await ytdl.getInfo(id);
+  const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+
+  if (audioFormats && audioFormats.length) {
+    return res.status(200).json({
+      success: true,
+      data: audioFormats,
+    });
+  } else {
+    res.status(502).json({
+      success: false,
+      message: 'Bad Gateway',
+      data: null,
+    });
+  }
+});
+
+// ii) Handling unavailable routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can not find ${req.originalUrl} on this server`,
+  });
+});
+
+// exporting app module
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+const app = require('./app');
+
+// Handling uncaught exception
+process.on('uncaughtException', (err) => {
+  console.info('UNCAUGHT EXCEPTION!');
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
+// setting path for .env file
+dotenv.config({ path: './config.env' });
+
+// Environment Logging
+
+// getting port number from environment
+const port = process.env.PORT || 8000;
+
+// creating a server
+const server = app.listen(port, () => {
+  if (process.env.NODE_ENV === 'production') {
+    console.log(`App is running on Production environment on port ${port}`);
+  } else {
+    console.log(`App is running on Development environment on port ${port}`);
+  }
+});
+
+// Handling unhandle promise rejection
+process.on('unhandledRejection', (err) => {
+  console.warn('UNHANDLED REJECTION!');
+  console.error(err.name, err.message);
+  server.close(() => process.exit(1));
+});
