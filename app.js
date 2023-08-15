4aaa93332f6f0d034dfd72e773fe9045d5cc4927
Sourav Mandal <sm227465@gmail.com>
2023-08-15 18:00:24 +0530

    info

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -16,21 +15,12 @@ dotenv.config({ path: './config.env' });
 // calling express function and store into a variable
 const app = express();
 
-// setting templating view engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
 // i) Implemented cors
 app.use(cors());
 app.options('*', cors());
 
 // ii) Set security HTTP headers
 app.use(helmet());
-
-// iii) Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 // iv) Limiting request from same IP (its help us from Bruteforce and DOS/DDOS attacks)
 const limiter = rateLimit({
@@ -52,6 +42,47 @@ app.use(hpp({ whitelist: [] }));
 // viii) Compressing
 app.use(compression());
 
+app.get('/api/v1/info/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide a YouTube video ID',
+    });
+  }
+
+  const result = ytdl.validateID(id);
+
+  if (!result) {
+    return res.status(404).json({
+      success: false,
+      message: `Sorry! Can not find YouTube video with this ID: ${id}`,
+    });
+  }
+
+  try {
+    const info = await ytdl.getInfo(id);
+
+    if (info.videoDetails) {
+      return res.status(200).json({
+        success: true,
+        details: info.videoDetails,
+      });
+    } else {
+      return res.status(502).json({
+        success: false,
+        message: 'Bad Gateway',
+        details: null,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error?.message || 'Something went wrong',
+    });
+  }
+});
 app.get('/api/v1/yt/:id', async (req, res) => {
   const { id } = req.params;
 
@@ -73,6 +104,7 @@ app.get('/api/v1/yt/:id', async (req, res) => {
 
   try {
     const info = await ytdl.getInfo(id);
+    console.log(info);
     const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
 
     if (audioFormats && audioFormats.length) {
